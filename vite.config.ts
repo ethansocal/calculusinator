41b67085fa3751dcddfe5f1cc59bb09fb1b83733
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-02-20 14:16:33 -0800

    Add asset_url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+const ASSET_URL = process.env.ASSET_URL || "";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -10,4 +11,5 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    base: `${ASSET_URL}/`,
 });
