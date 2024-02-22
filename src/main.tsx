7e80aedcf5be5867545ce4e821c27c113d3bfbee
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-02-22 13:01:34 -0800

    Add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,29 @@ import App from "./App";
 import { DevSupport } from "@react-buddy/ide-toolbox";
 import { ComponentPreviews, useInitial } from "./dev";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://4a0c291edbfc6e9a6d264098c565cf45@o1112946.ingest.sentry.io/4506792449409024",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [
+        "localhost",
+        /^https:\/\/ethansocal\.github\.io\/[\s\S]+/,
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <DevSupport
